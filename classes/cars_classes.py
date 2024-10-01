292d2a1fc762e8c438759a744c5e1befccb7e5d5
N2SuccessAll <mahamadtoumani@gmail.com>
2024-10-01 15:56:19 +0100

    Add Person and Car class.

---
+++ classes/cars_classes.py
@@ -0,0 +1,12 @@
+class Car:
+
+    def speed(self):
+        print("Ferrari 365 can get 280km per hour")
+
+def main():
+
+    ferrari_365 = Car()
+
+    ferrari_365.speed()
+
+main()
---
+++ classes/person_class.py
@@ -0,0 +1,17 @@
+class Person:
+    def eat(self):
+        print("I am eating banana!")
+
+    def run(self):
+        print("I am running at London Marathon")
+
+def main():
+
+    Sophie = Person()
+    Ludovic = Person()
+
+
+    Sophie.eat()
+    Ludovic.run()
+
+main()
